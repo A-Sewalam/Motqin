5ce2bc76591907aff1636ea098411274fecde098
Abdelhadi-Hesham-Elnehrawy <hadihesham27@gmail.com>
2026-01-25 00:15:57 +0200

    Implement SubjectsService and update Program.cs

--- Motqin/Services/SubjectsService.cs
+++ Motqin/Services/SubjectsService.cs
@@ -1,6 +1,25 @@
-﻿namespace Motqin.Services
+﻿using Microsoft.EntityFrameworkCore;
+using Motqin.Data;
+using Motqin.Enums;
+using Motqin.Models;
+
+namespace Motqin.Services
 {
-    public class SubjectsService
+    public interface ISubjectsService
     {
+        Task<IEnumerable<Subject>> GetAllAsync(string country, GradeLevel gradeLevel, EducationalStage educationalStage);
+    }
+
+    public class SubjectsService : ISubjectsService
+    {
+        private readonly AppDbContext context;
+
+        public SubjectsService(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IEnumerable<Subject>> GetAllAsync(string country, GradeLevel gradeLevel, EducationalStage educationalStage) =>
+            await context.Subjects.Where(s =>s.Country == country && s.GradeLevel == gradeLevel && s.EducationalStage == educationalStage).AsNoTracking().ToListAsync();
     }
 }
